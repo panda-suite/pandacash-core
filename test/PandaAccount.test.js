b06aaeb5e89035b99bf5a0e0e855a1127884cdda
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-11-12 17:35:55 +0100

    adjusted tests

--- test/PandaAccount.test.js
+++ test/PandaAccount.test.js
@@ -9,8 +9,8 @@ describe('PandaAccount', () => {
 
         assert.strictEqual(account.keyPairs.length, noOfkeyPairs);
         assert.strictEqual(account.keyPairs[0].bcash.address, "bchreg:qqmqn5nwce5fatsjc6nxdh7utpr7ytc6xqyvrdhxva");
-        assert.strictEqual(account.keyPairs[0].bcash.privateKey, "EQc3gu9Wg6wW4svhqbJjBS3fubUBysM2QHz7xe1PfQmvNhPv2jXT");
-        assert.strictEqual(account.keyPairs[0].standard.address, "RECvLKVTx3xXwq8XxCeWwoFcchPxgaZJym");
+        assert.strictEqual(account.keyPairs[0].bcash.privateKey, "cSzzswqYm1SzFyuAEt7noN9EzLQPNDkLf7oYLKdVpi4yL1yv735q");
+        assert.strictEqual(account.keyPairs[0].standard.address, "mkSgYrhAAFbDewExCbdmgC8jiRY4wP7CWn");
         // assert.strictEqual(account.keyPairs[0].standard.privateKey, "a1afb2b6a8272737ce5fae809c5f0859865934c9ffaccced8e1cf5149608e435");
     });
 });
