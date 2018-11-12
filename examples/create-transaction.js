8d728a09ef33bdf6fc4fe7133afa1274d12ff9af
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-11-12 18:16:00 +0100

    prettyf

--- examples/create-transaction.js
+++ examples/create-transaction.js
@@ -32,47 +32,13 @@ const server = panda.server({
     bch.Networks.enableRegtest();
 
     const privateKey = new bch.PrivateKey(pandaCashCore.account.keyPairs[0].privateKey, "regtest");
-    console.log(pandaCashCore.account.keyPairs[0].legacyAddress);
+
     const transaction = new bch.Transaction()
       .from(utxo)
       .to(pandaCashCore.account.keyPairs[0].legacyAddress, 15000)
       .sign(privateKey);
     
-    console.log(transaction.toString()) // 01000000018689302ea03ef...
+    console.log(transaction.toString());
 
-    /**
-    const transactionBuilder = new BITBOX.TransactionBuilder("regtest");
-
-    const satoshisToSend = 1000 // <--- This is where you set the amount to send.
-    const originalAmount = utxo.satoshis
-  
-    // add input with txid and index of vout
-    transactionBuilder.addInput(utxo.txid, utxo.vout)
-
-    // amount to send back to the sending address.
-    // It's the original amount - 1 sat/byte for tx size
-    const remainder = originalAmount - satoshisToSend - 0.00001;
-
-    console.log(pandaCashCore.account.keyPairs[0].cash);
-
-    // add output w/ address and amount to send
-    transactionBuilder.addOutput(pandaCashCore.account.keyPairs[0].cash.address, remainder);
-    transactionBuilder.addOutput(pandaCashCore.account.keyPairs[1].cash.address, satoshisToSend);
-    
-    transactionBuilder.sign(
-        0,
-        keyPair,
-        redeemScript,
-        transactionBuilder.hashTypes.SIGHASH_ALL,
-        originalAmount
-    )
-
-    // build tx
-    const tx = transactionBuilder.build();
-    // output rawhex
-    const hex = tx.toHex()
-
-    console.log(hex);
-         */
     process.exit();
 })();
